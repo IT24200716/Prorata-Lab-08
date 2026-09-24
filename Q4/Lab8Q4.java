08d89829cb38b9021bea3fcdae29fb948f9dc3a5
IT24200716 <It24200716@my.sliit.lk>
2026-09-24 21:36:21 +0530

    Lab8Q4

---
+++ Q4/Lab8Q4.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Lab8Q4 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int[] studentsArray = new int[8];
+
+        // Input 8 Student IDs
+        for (int i = 0; i < 8; i++) {
+
+            System.out.print("Enter Student ID " + (i + 1) + ": ");
+            int id = input.nextInt();
+
+            if (id <= 0) {
+                System.out.println("Error! Student ID must be positive.");
+                i--; // repeat this position
+            } else {
+                studentsArray[i] = id;
+            }
+        }
+
+        // Search for Student ID
+        System.out.print("Enter Student ID to search: ");
+        int searchID = input.nextInt();
+
+        boolean found = false;
+
+        for (int i = 0; i < 8; i++) {
+            if (studentsArray[i] == searchID) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("Student is Available");
+        } else {
+            System.out.println("Student is Not Available");
+        }
+
+        input.close();
+    }
+}
