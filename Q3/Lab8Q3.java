8a567f0400ba4843cef6c0912f0e1789e20817b5
IT24200716 <It24200716@my.sliit.lk>
2026-09-24 21:35:23 +0530

    Lab8Q3

---
+++ Q3/Lab8Q3.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Lab8Q3 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int[] numbers = new int[6];
+
+        // Input 6 positive numbers
+        for (int i = 0; i < 6; i++) {
+
+            System.out.print("Enter positive number " + (i + 1) + ": ");
+            int number = input.nextInt();
+
+            if (number <= 0) {
+                System.out.println("Error! Please enter a positive number.");
+                i--; // repeat this position
+            } else {
+                numbers[i] = number;
+            }
+        }
+
+        // Find maximum
+        int max = numbers[0];
+
+        for (int i = 1; i < 6; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+
+        System.out.println("Maximum number: " + max);
+
+        input.close();
+    }
+}
