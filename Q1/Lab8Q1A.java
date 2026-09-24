e4ba5ea5a325d1c85166a2c16ee9e3ca7da3a8f2
IT24200716 <It24200716@my.sliit.lk>
2026-09-24 21:34:06 +0530

    Lab8Q1

---
+++ Q1/Lab8Q1A.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Lab8Q1A {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int[] myArray = new int[5];
+
+        // Input 5 numbers
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            myArray[i] = input.nextInt();
+        }
+
+        // Print in reverse order
+        System.out.println("Array in reverse order:");
+
+        for (int i = 4; i >= 0; i--) {
+            System.out.print(myArray[i] + " ");
+        }
+
+        input.close();
+    }
+}
